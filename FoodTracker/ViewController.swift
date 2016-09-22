8a41457673239d462056e2398e188d5d691023b1
hahegeman <hahegeman@micds.org>
2016-09-22 14:31:16 -0500

    Expected Declartion error

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController , UITextFieldDelegate{
+class ViewController: UIViewController , UITextFieldDelegate,
+    UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-
-    
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
         override func viewDidLoad() {
         super.viewDidLoad()
         //Handle the text field's user input through delegate callbacks
@@ -36,7 +38,21 @@ class ViewController: UIViewController , UITextFieldDelegate{
     }
    
 //MARK: Actions
-    @IBAction func setDefaultLabelText(sender: UIButton) {mealNameLabel.text = "Defult Text"
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        //Hide the keyboard
+        
+        nameTextField.resignFirstResponder()
     }
+
+// UIImagePickerController is a view controller that lets a user pick media from their photo library.
+    let imagePickerController = UIImagePickerController()
+    
+
+    
+// Make sure ViewController is notified when the user picks an image.
+    imagePickerController.delegate = self
+    presentViewController(imagePickerController, animated: true, completion: nil)
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {mealNameLabel.text = "Defult Text"}
 }
 
